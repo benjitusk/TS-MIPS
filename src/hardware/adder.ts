0f6ea86c9ed87afe250a0217a3ddbec1ed833941
Scimonster <tehalmightyscimonster@gmail.com>
2024-06-20 15:36:58 +0300

    Fix access records

--- src/hardware/adder.ts
+++ src/hardware/adder.ts
@@ -6,8 +6,8 @@ import { Connector } from './connector';
  * Output 1 is the sum of the inputs, output 2 is the carry.
  */
 export class Adder<const TSize extends number> extends DualOutputComponent<TSize, 1> {
-    input1: Connector<TSize>;
-    input2: Connector<TSize>;
+    public readonly input1: Connector<TSize>;
+    public readonly input2: Connector<TSize>;
 
     constructor(size: TSize) {
         super(size, 1);
@@ -15,7 +15,7 @@ export class Adder<const TSize extends number> extends DualOutputComponent<TSize
         this.input2 = new Connector(size);
     }
 
-    _update() {
+    protected _update() {
         const sum = this.input1.getValue() + this.input2.getValue();
         this.output1.setValue(sum); // wire truncates the value to the correct size
         this.output2.setValue(sum >> this.output1.size);
